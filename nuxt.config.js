5a33f859ae2f565d720bf30b785b129e9af08392
Aleko <alex.kasongolenge@yahoo.com>
2020-11-21 19:19:29 -0800

    cleaned up code

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Nuxt API Example - Using Vuex, axios, and a REST API with Nuxt by Alex Kasongo' },
+      { property: 'og:title', content: 'Nuxt API Example' },
+      { property: 'og:description', content: 'Showing a collections of posts and using dynamic routes for individual posts with Nuxt.js Vuex, and an API.' },
+      // { property: 'og:image', content: 'https://nuxt-api-example.netlify.com/nuxt-api-example-meta-image.jpg' },
+      // { property: 'og:url', content: 'https://nuxt-api-example.netlify.com' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { property: 'og:site_name', content: 'Nuxt API Example' },
+      { name: 'twitter:image:alt', content: 'Screenshot of home page for demo site' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -17,7 +24,6 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~plugins/axios.js',
     { src: '~/plugins/persistentState.js', ssr: false },
   ],
 
